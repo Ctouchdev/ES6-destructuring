2ceff3ec552155a537c13db92048e547c71e7f02
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-20 10:31:20 -0500

    Updates

--- src/data.js
+++ src/data.js
@@ -1,4 +1,23 @@
 const animals = [
-  { name: "cat", sound: "meow" },
+  {
+    name: "cat",
+    sound: "meow",
+    feeding: {
+      food: "2 times per day",
+      water: "drinky water all days"
+    }
+  },
   { name: "dog", sound: "woof" }
 ];
+
+function useAnimals(animal) {
+  return [
+    animal.name,
+    function () {
+      console.log(animal.sound);
+    }
+  ];
+}
+
+export default animals;
+export { useAnimals };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,57 @@
-// CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+import animals, { useAnimals } from "./data";
+import cars from "./practice";
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+
+const [honda, tesla] = cars;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
+
+// const [cat, dog] = animals;
+
+// const {
+//   name,
+//   sound,
+//   feeding: { food, water }
+// } = cat;
+
+// // const { name: catName, sound: catSound } = cat;
+
+// // const { name, sound } = cat;
+
+// const [animal, makeSound] = useAnimals(cat);
+
+// console.log(animal);
